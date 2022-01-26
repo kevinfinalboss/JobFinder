f3a1f504cd33c1f5c01d63b6b79ca3a2baa67a17
Kevin Gomes <kevinmg50@gmail.com>
2022-01-26 14:51:37 -0300

    feita a conexão com o banco de dados

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const db = require('./db/connectiondb')
 
 const PORT = 3000
 
@@ -7,6 +8,19 @@ app.listen(PORT, function () {
     console.log(`Servidor rodando na porta ${PORT}`)
 })
 
+//db connection
+
+db
+  .authenticate()
+  .then(() => {
+      console.log("Conexão com o banco de dados efetuada")
+  })
+  .catch(err => {
+      console.log("Opa, algo deu errado na hora de conectar ao banco de dados", err)
+  })
+
+
+// routes
 app.get('/', (req, res) => {
     res.send("Aplicação funcionando")
 })

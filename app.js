8350d9b9a6b0628b4b69ac44c95e10b45d8790c0
Kevin Gomes <kevinmg50@gmail.com>
2022-01-27 11:14:42 -0300

    Renderização com handlebars feita

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+const app =  express()
 const path = require('path')
 const exphbs = require('express-handlebars')
 const db = require('./db/connectiondb')

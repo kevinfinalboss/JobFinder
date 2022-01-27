d68f109559747b6ade72d447d9c6ded5b4c2e34c
Kevin Gomes <kevinmg50@gmail.com>
2022-01-27 12:00:26 -0300

    dados do banco colocados na view

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const path = require('path')
+const path =  require('path')
 const exphbs = require('express-handlebars')
 const db = require('./db/connectiondb')
 const bodyParser = require('body-parser')
